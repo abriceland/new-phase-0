eff60949b66f68530f09b8913698c7637301155e
Alicia Briceland <alicia@singalexander.com>
2016-03-02 20:24:02 +0000

    Add files

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,17 @@
+# Define an Empty Method
+
+
+
+# I worked on this challenge by myself.
+
+
+
+# Your Solution Below
+
+
+def do_something(work, eat, exercise)
+    puts work
+    puts eat
+    puts exercise
+end
+
---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,31 @@
+# Math Methods
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ num_1 = 2
+ num_2 = 3
+ p num_1 + num_2
+end
+
+
+def subtract(num_1, num_2)
+ num_1 = 2
+ num_2 = 3
+ p num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ num_1 = 2
+ num_2 = 3
+ p num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ num_1 = 10.0
+ num_2 = 2
+ p num_1 / num_2
+end
